bf5a4d82b9ce7c6f70d572759bb2020794f0dcec
beatrizgomes <beatrizgomesxx@gmail.com>
2022-12-20 14:10:31 -0300

    no método "getUserByUsername_userIsValid_returnOk()" ocorre um problema de não encontrar o firstname do usuario. É retornado outro valor

--- src/test/java/User/UserTest.java
+++ src/test/java/User/UserTest.java
@@ -3,12 +3,17 @@ package User;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.example.entities.user.User;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.isA;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -35,23 +40,84 @@ public class UserTest {
 
     @BeforeEach
     void setRequest(){
-        requestSpecification = given().header("api-key", "special-key")
+        requestSpecification = given().config(RestAssured.config()
+                        .logConfig(logConfig()
+                                .enableLoggingOfRequestAndResponseIfValidationFails()))
+                .header("api-key", "special-key")
                 //.header("Content-Type","application/json");
                 .contentType(ContentType.JSON);
     }
 
     @Test
+    @Order(1)
     public void CreateNewUser_WithValidData_ReturnOk(){
        requestSpecification.body(user)
                .when().post("/user")
                .then()
                .assertThat().statusCode(200).and()
                .body("code", equalTo(200))
-               .body("type", equalTo("unknow"))
+               .body("type", equalTo("unknown"))
                .body("message", isA(String.class))
                .body("size()", equalTo(3));
     }
 
+    @Test
+    @Order(2)
+    void getLogin_valid_returnOk(){
+        requestSpecification
+                .param("username", user.getUsername())
+                .param("password", user.getPassword())
+                .when()
+                .get("user/login")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and().time(lessThan(2000L))
+                .and().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("loginResponseSchema.json"));
 
+    }
+
+    @Test
+    @Order(3)
+    void getUserByUsername_userIsValid_returnOk(){
+        requestSpecification
+                .when()
+                .get("/user/" + user.getUsername())
+                .then()
+                .assertThat().statusCode(200).and().time(lessThan(2000L))
+                .and().body("firstname", equalTo(user.getFirstname()));
+
+        //TO DO: Schema Validation
+
+    }
+
+    @Test
+    @Order(4)
+    void deleteUser_userExits_Return200(){
+        requestSpecification
+                .when()
+                .get("/user/" + user.getUsername())
+                .then()
+                .assertThat().statusCode(200).and().time(lessThan(2000L))
+                .log();
+
+    }
+
+
+    @Test
+    @Order(5)
+    void createUser_withInvalidBody_returnBadRequest(){
+        Response response = requestSpecification
+                .body("teste")
+                .when()
+                .post("/user")
+                .then()
+                .extract().response();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(400, response.statusCode());
+        Assertions.assertEquals(true, response.getBody().asPrettyString().contains("unknown"));
+        Assertions.assertEquals(3, response.body().jsonPath().getMap("$").size());
+    }
 
 }
